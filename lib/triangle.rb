361ea180869b03a7102d84f8dc18dab2c9509ebe
Carla <csachett@student.scf.edu>
2022-10-08 00:09:09 -0400

    commit tree ce51c098bc8781dbc6ff03c2f4d91d0d20233f8a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,75 @@
+# class Triangle
+#   # triangle code
+#   attr_reader :s1, :s2, :s3
+  
+#   def initialize(s1, s2, s3)
+#     @s1 = s1
+#     @s2 = s2
+#     @s3 = s3
+#   end
+#   def kind
+
+#     if (s1 == s2) && (s2 == s3)
+#        :equilateral
+#     elsif (s1 == s2) || (s2 == s3) || (s1 == s3)
+#         :isosceles
+#     else
+#       :scalene
+#    end
+#   end
+  
+#   def sides_greater_than_zero?
+#     [s1, s2, s3].all?(&:positive?)
+#   end
+
+#   def valid_triangle_inequality?
+#     s1 + s2 > s2 && s1 + s3 > s2 && s2 + s3 > s1
+#   end
+#   def validate_triangle
+#     raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+#   end
+#   class TriangleError < StandardError
+#     # triangle error code
+#   end
+# end
+
+
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
